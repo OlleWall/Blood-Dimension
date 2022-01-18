9dda1dae3514661b590236294e5288fec32e12c7
OlleWall <97678822+OlleWall@users.noreply.github.com>
2022-01-18 14:29:23 +0100

    camera follow script och sortering

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+
+    public GameObject cameraGO;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraGO.transform.position = new Vector3(transform.position.x, transform.position.y, cameraGO.transform.position.z);
+    }
+}
