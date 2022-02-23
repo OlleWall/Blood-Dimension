ba423f55ff70dffb94f4e4add10e29cb10dfce1d
yqt33 <niljas.hellgrenolbjorns@elev.ga.lbs.se>
2022-02-23 08:41:31 +0100

    Träd och kommentering

--- Assets/niljas/EnemyAI.cs
+++ Assets/niljas/EnemyAI.cs
@@ -32,7 +32,7 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerDirection = new Vector2
+        playerDirection = new Vector2 //Olle
             (
             player.transform.position.x - transform.position.x,
             player.transform.position.y - transform.position.y
@@ -40,10 +40,10 @@ public class EnemyAI : MonoBehaviour
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, playerDirection);
 
-        if (hit.transform != null && hit.collider.gameObject.tag == "Player" && hit.distance < FlashLightCheck(flashLight.enabled))
+        if (hit.transform != null && hit.collider.gameObject.tag == "Player" && hit.distance < FlashLightCheck(flashLight.enabled)) //Niljas
         {
             //setter.target = hit.transform;
-            aiPath.maxSpeed = 8;
+            aiPath.maxSpeed = 8; //Niljas
             patrolNode.transform.position = player.transform.position;
             manager.Play("Chase");
             print("Played audio and raycast got close enough");
@@ -53,7 +53,7 @@ public class EnemyAI : MonoBehaviour
             aiPath.maxSpeed = 5;
         }
 
-        if (Vector3.Distance(transform.position, patrolNode.transform.position) < 1)
+        if (Vector3.Distance(transform.position, patrolNode.transform.position) < 1) //Niljas
         {          
             Patrol();
         }
@@ -64,7 +64,7 @@ public class EnemyAI : MonoBehaviour
             setter.target = patrolNode.transform;
         }*/
 
-        if (hit.transform != null && hit.distance > 10 && hit.collider.gameObject.tag == "Player")
+        if (hit.transform != null && hit.distance > 10 && hit.collider.gameObject.tag == "Player") //Niljas
         {
             manager.Stop("Chase");
             print("stopped chase audio");
@@ -73,13 +73,13 @@ public class EnemyAI : MonoBehaviour
 
     public void Patrol()
     {
-        min = player.transform.position - new Vector3(-areaSize, -areaSize, 0);
-        max = player.transform.position - new Vector3(areaSize, areaSize, 0);
+        min = player.transform.position - new Vector3(-areaSize, -areaSize, 0); //Olle
+        max = player.transform.position - new Vector3(areaSize, areaSize, 0); //Olle
 
-        patrolNode.transform.position = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), 0);
+        patrolNode.transform.position = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), 0); //Olle
     }
 
-    int FlashLightCheck(bool enabled)
+    int FlashLightCheck(bool enabled) //Olle
     {
         if (enabled == true)
         {
@@ -91,7 +91,7 @@ public class EnemyAI : MonoBehaviour
         }    
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) //Niljas
     {
         if (collision.transform.tag == "Patrol")
         {
