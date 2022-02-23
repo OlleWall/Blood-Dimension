e07d435c1a59f8fa220fa9287009ffe595a47993
elsawinther <elsawinther@icloud.com>
2022-02-23 10:25:47 +0100

    komentar

--- Assets/Scripts/PlayerPause.cs
+++ Assets/Scripts/PlayerPause.cs
@@ -24,23 +24,25 @@ public class PlayerPause : MonoBehaviour
             }
         }
     }
+    //När Esc trycks ner ska spelat pausas om det inte redan är pausat för då ska det startas igen. - Elsa
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
+    
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
-
+    // Den här koden pausar respektive startar. 
     public void ExitToMenu()
     {
         SceneManager.LoadScene("MainMenu");
         Resume();
     }
-
+    //sen om man klickar på knappen som är "exit to main menu" så ska man bli skickad till main menu scenen.
 }
